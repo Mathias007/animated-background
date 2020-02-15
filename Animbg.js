695fcd6362aff25a00b92c08f9ba3053b80e0a06
Mathias007 <mathias.najlepszy@gmail.com>
2020-02-15 22:18:54 +0100

    Create Animbg class and use it in the project.

---
+++ Animbg.js
@@ -0,0 +1,26 @@
+class AnimBg {
+    constructor(selector) {
+        this.elements = document.querySelectorAll(selector);
+    }
+
+    listenCursorMove(event) {
+        const { clientX, clientY } = event;
+        const centerX = window.innerWidth / 2;
+        const centerY = window.innerHeight / 2;
+
+        const positionX = clientX - centerX;
+        const positionY = clientY - centerY;
+
+        this.elements.forEach(element =>
+            this.moveElement(element, positionX, positionY)
+        );
+    }
+
+    moveElement(element, positionX, positionY) {
+        const ratioX = -element.getAttribute("ratioX");
+        const ratioY = -element.getAttribute("ratioY");
+
+        element.style.transform = `translate(${positionX *
+            ratioX}px, ${positionY * ratioY}px)`;
+    }
+}
